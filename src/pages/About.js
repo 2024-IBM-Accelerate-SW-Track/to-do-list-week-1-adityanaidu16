add845f70f6b87b62641969b70a43c2c4abe7fb9
Aditya Naidu <adityanaidu16344@gmail.com>
2024-06-07 21:02:46 -0500

    week 1 submission

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
-  
+import "./About.css";
+import profilepic from "../assets/profilepic.jpg";
+
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div>
+          <div className="split left">
+          <div className="centered">
+          <img 
+          className="profile_image"
+          src={profilepic}
+          alt="Profile Pic"
+          ></img>
+          </div>
+          </div>
+          <div className="split right">
+          <div className="centered">
+          <div className="name_title">Aditya Naidu</div>
+          <div className="brief_description">
+          - Studying Computer Science @ Purdue University<br/>
+          - Software Engineering Intern @ Allthenticate<br/>
+          - Past Digital Security Intern @ H-E-B Digital<br/>
+          - Excited to experience IBM Accelerate and learn more about the tech industry!<br/>
+          </div>
+          </div>
+          </div>
+          </div>
       </div>
     )
   }
